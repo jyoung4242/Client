5f0d37eddc1818903dbc3952f7b122e6fc3bee14
Justin Young <justin_dean_young@yahoo.com>
2023-06-05 14:02:28 -0400

    dialog plugin completed

--- content/Scenes/game.ts
+++ content/Scenes/game.ts
@@ -1,5 +1,5 @@
 import { Scene } from "../../src/components/SceneManager";
-import { GameRenderer, RenderState } from "../../src/components/Renderer";
+import { GameRenderer, RenderState, RendererConfig } from "../../src/components/Renderer";
 import { Assets } from "@peasy-lib/peasy-assets";
 import { Kitchen } from "../Maps/kitchen";
 import { Player } from "../Game Objects/Player";
@@ -7,16 +7,35 @@ import { Counter } from "../Game Objects/Counter";
 import { Bookshelf } from "../Game Objects/Bookshelf";
 import { NPC1 } from "../Game Objects/npc1";
 import { OutsideMap } from "../Maps/outside";
+import { StoryFlagManager } from "../../src/components/StoryFlagManager";
+import { DialogManager } from "../PlugIns/DialogueManager";
 
 export class Game extends Scene {
   renderer = GameRenderer;
+
+  //****************************************** */
+  //dm name here is critical for peasy bindings
+  //when using plugins, be very careful how you access them
+  //****************************************** */
+  dm = new DialogManager();
+
   renderState = RenderState;
   storyFlags = {
-    someCondition: true,
+    someCondition: false,
+    metBookcase: false,
+    threat: false,
+    meek: false,
+    deaf: false,
+    angry: false,
   };
+  sm = new StoryFlagManager(this.storyFlags);
 
+  /**
+   * plug-ins are inserted after the renderer
+   */
   public template = `<scene-layer class="scene" style="width: 100%; height: 100%; position: absolute; top: 0; left:0; color: white;">
     ${this.renderer.template}
+    ${this.dm.template}
   </scene-layer>`;
 
   public async init() {
@@ -35,14 +54,27 @@ export class Game extends Scene {
     ]);
 
     //Initialize Renderer
-    this.renderer.initialize(this.renderState, 2, this.storyFlags, { width: 400, aspectratio: 3 / 2 });
+    const renderConfig: RendererConfig = {
+      state: this.renderState,
+      storyFlags: this.sm,
+      viewportDims: { width: 400, aspectratio: 3 / 2 },
+      objectRenderOrder: 2,
+      physicsFPS: 30,
+      renderingFPS: 60,
+    };
+    this.renderer.initialize(renderConfig);
 
     //Load Maps
     this.renderer.createMap([new Kitchen(Assets), new OutsideMap(Assets)]);
     this.renderer.changeMap("kitchen");
 
     //Load Objects
-    let objConfig = [new Player(Assets), new Counter(Assets), new Bookshelf(Assets), new NPC1(Assets)];
+    let objConfig = [
+      new Player(Assets, this.sm, this.dm),
+      new Counter(Assets, this.sm),
+      new Bookshelf(Assets, this.sm, this.dm),
+      new NPC1(Assets, this.sm, this.dm),
+    ];
     this.renderer.createObject(objConfig);
 
     //Set Camera
